f0cd42383efef766279c2f07cc94465ec5c6f8dc
Sergio Ordoñez <sergioo@nicasource.com>
2021-11-05 17:22:08 -0600

    feat: added common module to encapsulate app guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
 import * as Joi from '@hapi/joi';
 import appConfig from './config/app.config';
 
@@ -31,6 +32,7 @@ import appConfig from './config/app.config';
       }),
     }),
     CoffeeRatingModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
