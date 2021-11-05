30a8ea103226d35cc3f450b028a825720fefc8f4
Sergio Ordoñez <sergioo@nicasource.com>
2021-11-05 12:50:39 -0600

    feat: added app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,34 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
-    CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database host
-      username: 'postgres', // username
-      password: 'postgres', // user password
-      database: 'postgres', // name of our database,
-      autoLoadEntities: true, // models will be loaded automatically
-      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    ConfigModule.forRoot({
+      load: [appConfig],
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
     }),
+    CoffeesModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres', // type of our database
+        host: process.env.DATABASE_HOST, // database host
+        port: +process.env.DATABASE_PORT, // database host
+        username: process.env.DATABASE_USER, // username
+        password: process.env.DATABASE_PASSWORD, // user password
+        database: process.env.DATABASE_NAME, // name of our database,
+        autoLoadEntities: true, // models will be loaded automatically
+        synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+      }),
+    }),
+    CoffeeRatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
